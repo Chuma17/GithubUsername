34c2b02d237875dd983d13836c14e150d71c9f0e
Chukwuma <nwaigbochukwuma6@gmail.com>
2023-12-31 20:24:47 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -3,18 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { AuthProvider } from './context/AuthProvider';
 import { HashRouter, Routes, Route } from 'react-router-dom'; // Changed BrowserRouter to HashRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HashRouter> {/* Changed BrowserRouter to HashRouter */}
-      <AuthProvider>
         <Routes>
           <Route path='/*' element={<App />} /> {/* This line was added */}
         </Routes>
-      </AuthProvider>
     </HashRouter> {/* Changed BrowserRouter to HashRouter */}
   </React.StrictMode>
 );
